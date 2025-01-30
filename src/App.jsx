52cb702a1e4411bf9cb902315ac85868cb53816d
Rushikesh Patil <rushikesh1502@gmail.com>
2025-01-30 20:51:37 +0530

    React: Mini Project Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import ChangeColor from "./Components/01-ColorChange/ChangeColor"
-
+// import ChangeColor from "./Components/01-ColorChange/ChangeColor"
+import ApiFetch from "./Components/02-Users-Api-Fetch/ApiFetch"
 
 function App() {
 
   return (
     <>
-      <ChangeColor />
+      {/* <ChangeColor /> */}
+      <ApiFetch />
     </>
   )
 }
