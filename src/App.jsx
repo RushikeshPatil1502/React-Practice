69278a51b9ab211976a46a0be9575daf18afdc3c
Rushikesh Patil <rushikesh1502@gmail.com>
2025-02-01 19:50:57 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 // import ChangeColor from "./Components/01-ColorChange/ChangeColor"
 // import ApiFetch from "./Components/02-Users-Api-Fetch/ApiFetch"
-import Counter from "./Components/03-Counter/Counter"
+// import Counter from "./Components/03-Counter/Counter"
+
+import ChangeFontSize from "./Components/04-Font-Size-Change/ChangeFontSize"
 
 function App() {
 
   return (
-    <>
+    <div className="h-screen flex justify-center items-center">
       {/* <ChangeColor /> */}
       {/* <ApiFetch className=' mb-10' /> */}
-      <Counter />
-    </>
+      {/* <Counter /> */}
+      <ChangeFontSize />
+    </div>
   )
 }
 
