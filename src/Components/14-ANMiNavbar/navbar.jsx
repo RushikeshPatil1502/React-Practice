8d86eeb3ff645efa788e92a6ed8d2e25bb63ba98
Rushikesh Patil <rushikesh1502@gmail.com>
2025-02-19 11:22:35 +0530

    React: ANMi Project

--- src/Components/14-ANMiNavbar/navbar.jsx
+++ src/Components/14-ANMiNavbar/navbar.jsx
@@ -20,7 +20,7 @@ function ANMiNavbar() {
 
 
   return (
-    <div className="flex p-4 justify-between items-center text-center bg-purple-300">
+    <div className="flex flex-col gap-4 md:flex-row p-4 justify-between items-center text-center bg-purple-300">
         <div className="flex gap-4 justify-between items-center text-center w-full md:w-fit ">
             <h1 className="text-xl font-bold">ANMi</h1>
             <Link className="text-lg font-semibold" to={'ANMi-home-page'}>Home</Link>
@@ -28,10 +28,10 @@ function ANMiNavbar() {
         </div>
         {/* <div> */}
         <div className="flex justify-between items-center gap-2">
-          <form className="md:flex hidden justify-between items-center gap-2" 
+          <form className="flex justify-between items-center gap-2" 
           onSubmit={(event)=> handleOnclick(event)}>
             <input 
-            className="px-3 border rounded-xl md:flex hidden border-black text-gray-800" 
+            className="px-3 border rounded-xl border-black text-gray-800" 
             type="text" 
             placeholder="search"
             value={searchTerm}
